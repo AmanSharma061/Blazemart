1c0a9b1d204c554b564aa20782de2721c53e50d5
Aman Sharma <97701226+AmanSharma061@users.noreply.github.com>
2024-01-14 02:27:55 +0530

    Update route.js

--- app/api/category/route.js
+++ app/api/category/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import connectToDatabase from '../../database/connectDb'
+import connectToDatabase from '../../database/connectDb.js'
 import Category from '../../database/models/categoryModel'
 
 export async function POST (request) {
